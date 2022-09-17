4d3b0794937ffdf63091fdba3705b4a85c4ba257
John Steven Bernal Gonzalez <bernalgonzalezjohn@gmail.com>
2022-09-17 16:32:24 -0500

    Inicializando proyecto en SpringBoot

---
+++ src/main/java/com/fundamentospring/springboot/fundamentospringboot/FundamentospringbootApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentospring.springboot.fundamentospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentospringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentospringbootApplication.class, args);
+	}
+
+}
